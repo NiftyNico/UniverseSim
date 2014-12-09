23115c3d2cc8bf363461be7827545e6a6d78ab29
NiftyNico <nicohiguera11@gmail.com>
2014-12-09 11:25:28 -0800

    lighting const

--- src/shader/frag_shader.glsl
+++ src/shader/frag_shader.glsl
@@ -16,9 +16,9 @@ void main()
 	vec3 kd = texture2D(texture0, fragTexCoords0).rgb;
 	vec3 atmosKd = texture2D(texture2, fragTexCoords1).rgb;
 	vec4 theColor = vec4(0.0, 0.0, 0.0, 1.0);
-
+	vec4 maxColor = vec4(kd + atmosKd, 1.0);
 	if (isSun != 0) {
-		theColor = vec4(kd + atmosKd, 1.0);		
+		theColor = maxColor;		
 	} else {			
 		vec3 n = normalize(normal);
 		int i;
@@ -36,8 +36,9 @@ void main()
 			vec4 atmosColor = vec4(atmosKd, 1.0) * lighting;
 			vec4 mainColor = vec4(kd, 1.0) * lighting + vec4(ks, 1.0) * pow(max(dot(h, n), 0.0), 10.0);
 			vec4 newColor = atmosColor + mainColor;
-			theColor += newColor / float(numLights);
+			theColor += newColor;
 		}
+		theColor = min(maxColor, theColor);
 	}
 
 	gl_FragColor = theColor;
